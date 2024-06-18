13f9ef225eb88f9b1d6fc812153c0d046dcca775
Mahari9 <merytseg77@gmail.com>
2024-06-18 06:17:08 -0700

    Add db to offer class

--- models/offer.py
+++ models/offer.py
@@ -2,19 +2,33 @@
 """This module defines the offer class for Easy Freight"""
 
 from datetime import datetime
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 
-class Offer(BaseModel):
+class Offer(BaseModel, Base):
     """
     Represents an offer made by a carrier for a shipment.
     Inherits from BaseModel for common attributes and methods.
     """
 
-    id = str  # Offer ID (primary key)
-    shipment_id = str  # Shipment ID (foreign key references Shipment.id)
-    carrier_id = str  # Carrier ID (foreign key references User.id)
-    price = float  # Price offered by the carrier
-    estimated_delivery_time = int  # Estimated delivery time in days
-    submitted_at = datetime  # Submission Date
+    if models.storage_t == 'db':
+        __tablename__ = 'offers'
+        shipment_id = Column(String(60), ForeignKey('shipment_id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        price = Column(Float, nullable=False)
+        estimated_delivery_time = Column(Integer, nullable=False)
+        submitted_at = Column(datetime, nullable=False)
+
+    else:
+        id = ""  # Offer ID (primary key)
+        shipment_id = ""  # Shipment ID (foreign key references Shipment.id)
+        carrier_id = ""  # Carrier ID (foreign key references User.id)
+        price = 0.0  # Price offered by the carrier
+        estimated_delivery_time = 0  # Estimated delivery time in days
+        submitted_at = ""  # Submission Date
 
     def __init__(self, *args, **kwargs):
         """
